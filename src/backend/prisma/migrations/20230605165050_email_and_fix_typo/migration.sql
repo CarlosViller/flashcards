b260abe172fb4f9e3dffce27fc429efe39483160
Deltaaaaq <carlos.viller98@gmail.com>
2023-06-05 20:56:27 -0300

    add email to user

---
+++ src/backend/prisma/migrations/20230605165050_email_and_fix_typo/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `anwser` on the `Card` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `answer` to the `Card` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Card" DROP COLUMN "anwser",
+ADD COLUMN     "answer" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "email" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
