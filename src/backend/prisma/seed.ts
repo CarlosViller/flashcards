aba8232d0de4f56b8bdaa121a1e70373cc6ac69b
Deltaaaaq <carlos.viller98@gmail.com>
2023-06-05 20:56:09 -0300

    create seed

---
+++ src/backend/prisma/seed.ts
@@ -0,0 +1,44 @@
+import prisma from "./client";
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      boxes: {
+        create: {
+          cards: {
+            create: [
+              {
+                question: "Pregunta 1",
+                answer: "Respuesta 1",
+              },
+              {
+                question: "Pregunta 2",
+                answer: "Respuesta 2",
+              },
+              {
+                question: "Pregunta 3",
+                answer: "Respuesta 3",
+              },
+              {
+                question: "Pregunta 4",
+                answer: "Respuesta 4",
+              },
+            ],
+          },
+        },
+      },
+    },
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
