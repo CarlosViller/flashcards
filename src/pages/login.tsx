611f7f68263f177b249273edcb5a1430c790a896
Carlos <carlos.viller98@gmail.com>
2023-08-16 23:46:13 -0300

    fix wording

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,7 +6,7 @@ export default function AuthPage() {
     <div className="bg-primary h-screen w-screen flex justify-between">
       <div className="text-white mx-10 pt-[120px]">
         <h1 className="text-9xl font-semibold">Kartu</h1>
-        <p>An application to create flashcards.</p>
+        <p>An application to create and share flashcards.</p>
       </div>
       <div className="h-scree w-1/3 bg-white shadow-xl pt-[150px]">
         <Auth />
