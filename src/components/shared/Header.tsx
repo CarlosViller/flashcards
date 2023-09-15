deeb0b6c2a1bbf6ddee62b338a39e0dff95bf9ce
Deltaaaaq <carlos.viller98@gmail.com>
2023-09-15 20:59:18 -0300

    clean header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,32 +1,36 @@
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import ProfilePic from "./ProfilePic";
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SearchBar from "./SearchBar";
 import { useRouter } from "next/router";
+import { useMediaQuery } from "@/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   const { data: session } = useSession();
   const router = useRouter();
 
+  const isMobile = useMediaQuery(500);
+
   if (router.pathname === "/login") return null;
 
   return (
-    <header className="relative top-0 h-10 bg-primary flex items-center justify-between px-5 shadow-sm">
+    <header className="relative top-0 h-10 bg-primary flex items-center justify-between lg:px-5 px-2 shadow-sm">
       <div className="flex items-center gap-8">
         <Link href="/" className="text-white font-semibold text-xl">
           Kartu
         </Link>
-        <SearchBar />
+        {!isMobile && <SearchBar />}
       </div>
-      <div className="flex items-center gap-5">
-        <Link
-          href="/create"
-          className="create-button text-white px-4 rounded-md border-white border-2"
-        >
-          Create
-        </Link>
+      <section className="flex items-center gap-4">
+        <button onClick={() => console.log("press")}>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="text-white"
+            size="lg"
+          />
+        </button>
         <Link href="/profile/me" className="flex items-center">
           <ProfilePic
             image={session?.user?.image}
@@ -34,13 +38,7 @@ export default function Header() {
             size={55}
           />
         </Link>
-        <button
-          onClick={() => signOut()}
-          className=" text-white flex items-center"
-        >
-          <FontAwesomeIcon icon={faRightFromBracket} />
-        </button>
-      </div>
+      </section>
     </header>
   );
 }
