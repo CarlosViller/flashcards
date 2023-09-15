328f816bd1c9c7213bc0ab108b362ab678043bad
Deltaaaaq <carlos.viller98@gmail.com>
2023-09-15 20:58:14 -0300

    move logout button to profile page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,6 +5,7 @@ import GridSection from "./shared/GridSection";
 import { CardBoxWithCards, SessionUser } from "@/types";
 import { ToastContext } from "@/ToastContext";
 import MiniBox from "./MiniBox";
+import { signOut } from "next-auth/react";
 
 interface Props extends SessionUser {
   id: string;
@@ -34,14 +35,18 @@ export default function Profile({ user, id }: Props) {
     fetchBoxes();
   }, [fetchBoxes]);
 
-
   return (
     <div>
       <div className="flex flex-col items-center pt-10 text-center gap-4 container mx-auto">
         <ProfilePic size={100} image={user.image} />
         <div>
           <h1 className="text-2xl">{user.name}</h1>
-          
+          <button
+            onClick={() => signOut()}
+            className="w-max mx-auto mt-1 text-red-600 border-red-600 border-2 rounded px-2 flex items-center"
+          >
+            Logout
+          </button>
         </div>
         <div className="flex justify-between gap-10">
           <div className="flex flex-col">
